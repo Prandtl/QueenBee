6e90ad736a965c46b3d60ee03bcc621cbb9db247
Prandtl <vasiliy.uchanev@prandtl.co>
2018-06-26 22:50:00 +0000

    parsing values

--- main.py
+++ main.py
@@ -1,15 +1,26 @@
 import serial
 
-def make_important_stuff():
-    for i in range(0,1000000):
-        if i % 100000 == 0:
-            print(i)
+def parse(raw):
+    stripped = raw.strip()
+    values = stripped.split(',')
+    print(len(values))
+    if len(values) != 3:
+        raise Exception('wrong amount of values')
+    return values
+
+def make_particle_filter_step(data):
+    for i in data:
+        print(i)
+    print('---------------')
 
 with serial.Serial('/dev/ttyUSB0', 19200, timeout=1) as ser:
     while True:
         ser.reset_input_buffer()
-        line = ser.readline()
-        print(line)
-        make_important_stuff()
-        print('-------------')
+        ser.readline()
+        line = ser.readline().decode("utf-8")
+        try:
+            data = parse(line)
+            make_particle_filter_step(data)
+        except:
+            print('[D]caught')
 
